fea2b5ddf94f8b819d87a59e99bd0ff91c381682
Ujjwal Katiyar <ukcode07@gmail.com>
2026-02-03 20:30:15 +0530

    refactor ui

--- app/src/dashboard/components/live-prices.tsx
+++ app/src/dashboard/components/live-prices.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LivePrices = () => {
   return (
     <>
-      <div className="w-full py-5 h-max text-white bg-[#0b0e11] mt-4 rounded-sm ">
+      <div className="px-2 py-5 h-max text-white bg-[#0b0e11] mt-4 rounded-sm ">
         <h1 className="mx-3">Asset Prices</h1>
         <hr className="text-gray-700 mx-3 mt-2" />
         <div className="flex  justify-center mt-3">
@@ -27,7 +27,7 @@ const LivePrices = () => {
               </tr>
               <tr className="text-gray-300 text-right">
                 <td className="px-2 min-w-10">BTCUSDT</td>
-                <td className="px-2 min-w-10 ">0.0015</td>
+                <td className="px-2 min-w-10 ">0.000015</td>
                 <td className="px-2 min-w-10">199.302369</td>
               </tr>
               <tr className="text-gray-300 text-right">
