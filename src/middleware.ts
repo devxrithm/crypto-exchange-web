cc36636e19b41529fea672fce62aedbc6a509d86
Ujjwal Katiyar <ukcode07@gmail.com>
2026-02-05 18:47:09 +0530

    add middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function middleware(request) {
+  const { pathname, origin } = request.nextUrl;
+
+  const accessTokenCookie = (await cookies()).get("accessToken");
+  const token = accessTokenCookie ? accessTokenCookie.value : null;
+  // const token = request.cookies.get("accessToken")?.value || null;
+
+  console.log(token);
+
+  const isPublicPath =
+    pathname === "/auth/login" || pathname === "/auth/signup";
+
+  // If already logged in, block access to login/signup
+  if (isPublicPath && token) {
+    return NextResponse.redirect(`${origin}/in/spot/btcusdt`);
+  }
+
+  // If not logged in, block access to protected routes
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(`${origin}/auth/login`);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/auth/login", "/auth/signup", "/in/spot/:pair*"],
+};
