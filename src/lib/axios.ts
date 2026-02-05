01f7f0b1fa971e77ff3d32573791e97bb223ecbd
Ujjwal Katiyar <ukcode07@gmail.com>
2026-02-05 17:29:34 +0530

    add configuration for axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,43 @@
 import axios from "axios";
+import { cookies } from "next/headers";
+import { useRouter } from "next/navigation";
 
 export const api = axios.create({
   baseURL: "https://api.example.com",
   withCredentials: true,
 });
+const cookieStore = await cookies();
 
-axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+axios.interceptors.request.use(async (config) => config);
+
+// config holds the data like
+//    {
+//   url: "/users/profile",
+//   method: "get",
+//   baseURL: "https://api.example.com",
+//   headers: {
+//     Authorization: "Bearer OLD_ACCESS_TOKEN"
+//   },
+//   withCredentials: true,
+//   timeout: 0,
+//   data: undefined
+// }
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+    const router = useRouter();
+    if (error.response?.status === 401) {
+      try {
+        await api.post("/auth/refreshToken");
+        return api(originalRequest);
+      } catch {
+        cookieStore.delete("accessToken");
+        router.push("/login");
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
