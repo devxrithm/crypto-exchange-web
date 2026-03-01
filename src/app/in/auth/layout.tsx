1a85ebbe8e7b97e2be816d10261c7e1598ffe55a
Ujjwal Katiyar <ukcode07@gmail.com>
2026-03-01 20:30:24 +0530

    refactor: replace useEffect with useLayoutEffect for token verification in ProtectedLayout

--- src/app/in/auth/layout.tsx
+++ src/app/in/auth/layout.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { api } from "@/src/lib/axios";
-import { useEffect } from "react";
+import { useLayoutEffect, useState } from "react";
+import axios from "axios";
 
 export default function ProtectedLayout({
   children,
@@ -9,11 +10,24 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
-  useEffect(() => {
+  const [error, setError] = useState("");
+
+  useLayoutEffect(() => {
     const verifyToken = async () => {
-      const res = await api.post("/api/auth/verify-token");
-      if (res.data.statusCode === 200) {
-        router.push("/in/spot/btcusdt");
+      try {
+        const res = await api.post("/api/auth/verify-token");
+        if (res.data.statusCode === 200) {
+          router.push("/in/spot/btcusdt");
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          router.push("/in/auth/login");
+          setError(
+            error.response?.data?.message || "Login failed. Please try again.",
+          );
+        } else {
+          setError("Something went wrong. Please try again.");
+        }
       }
     };
     verifyToken();
