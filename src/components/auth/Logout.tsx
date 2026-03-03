ed83ce41b4cffcb3410d45325dcd7ca8fab63664
Ujjwal Katiyar <ukcode07@gmail.com>
2026-03-03 19:36:05 +0530

    refactor: enhance error handling in Logout component for improved user feedback

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -4,8 +4,11 @@ import { api } from "@/src/lib/axios";
 import { logout } from "@/src/context/features/authSlice";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
+import axios from "axios";
+import { useState } from "react";
 
 const Logout = () => {
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -15,13 +18,20 @@ const Logout = () => {
       router.push("/in/auth/login");
       dispatch(logout());
     } catch (error) {
-      console.error("Logout error:", error);
+      if (axios.isAxiosError(error)) {
+        setError(
+          error.response?.data?.message || "Login failed. Please try again.",
+        );
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
     }
   };
 
   return (
     <>
       <button onClick={LogoutHandler}>
+        {error && <p className="text-red-400 text-xs px-3 py-2">{error}</p>}
         <div className="cursor-pointer px-8 py-1.5 rounded-md text-sm font-medium border border-slate-800 text-white">
           Logout
         </div>
