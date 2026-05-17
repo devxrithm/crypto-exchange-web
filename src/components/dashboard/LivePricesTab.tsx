8a5e0408a518be0a85dc37fcf645fb4094d8f90a
Ujjwal Katiyar <ukcode07@gmail.com>
2026-05-18 00:10:52 +0530

    Update WebSocket URL for LivePricesTab component

--- src/components/dashboard/LivePricesTab.tsx
+++ src/components/dashboard/LivePricesTab.tsx
@@ -23,7 +23,7 @@ const LivePrices = () => {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    const ws = new window.WebSocket("wss://fstream.binance.com/stream?streams=!markPrice@arr");
+    const ws = new window.WebSocket("wss://fstream.binance.com/market/ws/!markPrice@arr");
 
     ws.onopen = () => setConnected(true);
 
